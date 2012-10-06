0a4ed919c21e3f53b696155f30244cec489efaee
Rob Landers <landers.robert@gmail.com>
2012-10-06 13:01:54 -0400

    Added basic php files

---
+++ abundatrade_pugin.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @package Abundatrade Plugin
+ * @version 1.0
+ * @author Robert Landers (landers.robert@gmail.com)
+ */
+/*
+Plugin Name: Abundatrade Plugin
+Plugin URI: http://wordpress.org/extend/plugins/moneypress-abundatrade/
+Description: Get the value of items easily and make some money.
+Author: withinboredom
+Version: 1.0
+Author URI: http://withinboredom.info
+ */
+
+if (!defined("WP_CONTENT_DIR")) exit();
+
+class Abundatrade_withinboredom {
+    
+    private $folders;
+    
+    public $settings;
+    
+    /**
+     * Get all our folders and return them
+     */
+    public function getFolders($folders )
+    {
+        return array_merge_recursive($this->folders, $folders);
+    }
+    
+    /**
+     * Applies the configuration of the plugin
+     * @var array $config The config that is to be edited
+     * @return array The new config
+     */
+    public function applyConfig($config) {
+        $config['tabs'] = array(
+            0 => array( 'Settings', 'tabs__settings_withinboredom'),
+            1 => array('About', 'tabs__about_withinboredom'),
+            );
+        $config['help'] = array(
+            'settings' => array(
+                    'Overview' => "Stuff here",
+                )
+            );
+        $config['config'] = array(
+                'settings' => true,
+                'page_title' => 'Abundatrade Plugin',
+                'button_title' => 'Abundatrade',
+                'slug' => 'abundatrade',
+                'shortcodes' => array(
+                    'abundatrade'
+                ),
+            );
+        return $config;
+    }
+    
+    private function shortcode($atts) {
+    }
+    
+    /**
+     * Creates a map structure
+     */
+    function __construct() {
+        $this->BuildFolderList();
+        
+        add_filter("abundatrade(getFolders)", array(&$this, "getFolders"), 1);
+        add_filter("abundatrade(applyConfig)", array(&$this, "applyConfig"), 1);
+        add_filter("abundatrade(shortcode(abundatrade))", array(&$this, "shortcode"), 1);
+        
+        spl_autoload_register(array($this, "autoload"));
+        
+        $skel = new skel__skel();
+        
+        //load settings last for updates
+        $this->settings = new skel__settings();
+    }
+    
+    /**
+     * Autoloads all classes as required to only load in what we need
+     * @var string $classname The name of the class to load
+     */
+    static public function autoload($classname) {
+        $file = str_replace("__", "/", $classname);
+        $folders = apply_filters("abundatrade(getFolders)", array());
+        if (file_exists($folders['PluginDir'] . $file . ".php"))
+            include_once($folders['PluginDir'] . $file . ".php");
+        else
+            echo "file no exists: " . $folders['PluginDir'] . $file . ".php\n";
+    }
+    
+    /**
+     * Builds a list of folders for later distribution so we can find ourselves
+     */
+    private function BuildFolderList() {
+        $this->folders = array();
+        $this->folders['PluginDir'] = plugin_dir_path(__FILE__);
+        $this->folders['PluginUrl'] = plugins_url('', __FILE__);
+        $this->folders['PluginAdmin'] = admin_url() . 'admin.php?page=';
+        $this->folders['Basename'] = plugin_basename(__FILE__);
+    }
+}
+
+$GLOBALS['Abundatrade_withinboredom'] = new Abundatrade_withinboredom();
+
+//we don't want to autoload later on
+//spl_autoload_unregister(array($GLOBALS['TheMap_withinboredom'], 'autoload'));
---
+++ skel/skel.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * SKEL is a ui managment library
+ * @author Robert Landers (landers.robert@gmail.com)
+ * @copyright 2012 GPLV2+
+ * @package Abundatrade
+ * @subpackage skel
+ * @version 0.1
+ */
+
+if (!defined("WP_CONTENT_DIR")) exit();
+
+class skel__skel
+{
+    /**
+     * The configuration of skel stored
+     */
+    private $config;
+    
+    /**
+     * Lays out the skel framework
+     */
+	function __construct()
+    {
+    	$this->config = apply_filters("abundatrade(applyConfig)", array());
+        echo "Incredible<pre>";
+        var_dump($this->config);
+        
+        if (isset($this->config['config'])) {
+            if (isset($this->config['config']['settings']) && $this->config['config']['settings']) {
+                if (is_admin()) {
+                    
+                    //now we add a settings menu item
+                    add_action('admin_menu', array($this, 'buildSettings'));
+                }
+            }
+        }
+        
+        echo "</pre>";
+    }
+    
+    /**
+     * Displays the admin page for a given tab, and defaults to tab 0
+     * Calls stuff
+     */
+    public function display() {
+        if(!isset($_GET['tab']))
+        {
+            $_GET['tab'] = 1;
+        }
+        
+        echo "viewing: " . $this->config['tabs'][0][1] . " <- here";
+        $display = new $this->config['tabs'][0][1];
+        $display->settings();
+    }
+    
+    /**
+     * Builds the settings menu stuff
+     */
+    public function buildSettings() {
+        add_options_page($this->config['config']['page_title'], $this->config['config']['button_title'], 'manage_options', $this->config['config']['slug'], array($this, "display"));
+    }
+}
