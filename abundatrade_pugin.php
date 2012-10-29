eae35f980d9b1551e0caef053ec56e4493446564
Rob Landers <landers.robert@gmail.com>
2012-10-29 16:41:41 -0400

    Auto add the upload id if that's what we want to display

--- abundatrade_pugin.php
+++ abundatrade_pugin.php
@@ -159,7 +159,11 @@ class abundatrade_withinboredom {
         wp_enqueue_style("abundatrade_classic");
         wp_enqueue_script("abundatrade_remote");
         wp_enqueue_script("abundatrade_impromptu");
-        wp_localize_script('abundatrade_remote','abundacalc',array('server' => 'dev.abundatrade.com', 'url' => $this->folders['PluginUrl'], 'thanks' => $this->settings->thankyou_page)); 
+        $abundacalc = array('server' => 'dev.abundatrade.com', 'url' => $this->folders['PluginUrl'], 'thanks' => $this->settings->thankyou_page);
+        if (isset($_REQUEST['upload_id']) && $_REQUEST['upload_id'] != '') {
+            $abundacalc['upload_id'] = $_REQUEST['upload_id'];
+        }
+        wp_localize_script('abundatrade_remote','abundacalc',$abundacalc);
     }
     
     /**
