cf7c79d05efb78297c0d5bdb1ab7369baa4ef5cc
Robert Landers <landers.robert@gmail.com>
2013-09-30 12:08:46 -0400

    Fix styling issue

--- abundatrade_pugin.php
+++ abundatrade_pugin.php
@@ -455,7 +455,7 @@ class abundatrade_withinboredom {
 		</table>';
 
         $very_bottom = '
-		<div id="very_bottom" style="'.$hide.'" class="calcbg2 bottomcurve"><div style="float:right; padding-top:8px; padding-right:13px;" class="second_content_sec4">
+		<div id="very_bottom" style="'.$hide.'" class="calcbg2 bottomcurve"><div style="float:right; padding-top:8px; padding-right:18px;" class="second_content_sec4">
                 <a id="submitList" class="btnbg2 btn_link2" onclick="submit_the_list(this);">Get a quote</a>
             </div></div>';
         
