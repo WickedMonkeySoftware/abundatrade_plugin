7e0ca1576855ab94aa8f1418eb4984b1945eb9d2
Robert Landers <landers.robert@gmail.com>
2013-11-09 00:51:51 -0500

    Add token to every request

--- js/remote.js
+++ js/remote.js
@@ -83,7 +83,7 @@ function abundatrade_logout() {
 
     var request = jQuery.ajax({
         type: 'POST',
-        url: sec() + abundacalc.server + '/trade/process/user/logout/',
+        url: sec() + abundacalc.server + '/trade/process/user/logout/' + "?token=" + jQuery.cookie("PHPSESSID"),
         dataType: 'jsonp'
     });
     request.done(function (data) {
@@ -141,7 +141,7 @@ function get_login_status(secret) {
         var request = jQuery.ajax(
             {
                 type: 'POST',
-                url: sec() + abundacalc.server + '/trade/process/user/status/',
+                url: sec() + abundacalc.server + '/trade/process/user/status/' + "?token=" + jQuery.cookie("PHPSESSID"),
                 dataType: 'jsonp'
             }
         );
@@ -179,7 +179,7 @@ function clear_session(obj) {
                             {
                                 type: 'GET',
                                 url: sec() + abundacalc.server + '/trade/process/request.php',
-                                data: 'action=clear_session&a=' + jQuery("#a").val(),
+                                data: 'action=clear_session&a=' + jQuery("#a").val() + "&token=" + jQuery.cookie("PHPSESSID"),
                                 dataType: 'jsonp'
                             });
 
@@ -211,7 +211,7 @@ function report_error(error_function, response) {
                 var request = jQuery.ajax({
                     type: 'POST',
                     url: sec() + abundacalc.server + '/trade/process/error.php',
-                    data: 'error=' + error_function + '&response=' + escape(response.responseText) + '&app=' + navigator.appVersion + '&loc=' + escape(location.href),
+                    data: 'error=' + error_function + '&response=' + escape(response.responseText) + '&app=' + navigator.appVersion + '&loc=' + escape(location.href) + "&token=" + jQuery.cookie("PHPSESSID"),
                     dataType: 'jsonp'
                 });
             }
@@ -231,7 +231,7 @@ function new_session(this_link) {
         {
             type: 'GET',
             url: sec() + abundacalc.server + '/trade/process/request.php',
-            data: 'action=new_session&a=' + jQuery("#a").val(),
+            data: 'action=new_session&a=' + jQuery("#a").val() + "&token=" + jQuery.cookie("PHPSESSID"),
             dataType: 'jsonp'
         });
     request.done(function (data) { });
@@ -449,7 +449,7 @@ function delete_the_row(obj) {
     var request = jQuery.ajax(
         {
             type: 'GET',
-            url: sec() + abundacalc.server + '/trade/process/deleteItem.php',
+            url: sec() + abundacalc.server + '/trade/process/deleteItem.php' + "&token=" + jQuery.cookie("PHPSESSID"),
             data: 'product=' + product_code,
             dataType: 'jsonp'
         });
@@ -487,7 +487,7 @@ function load_previous_session(pretty, ignore_errors) {
         {
             type: 'GET',
             url: sec() + abundacalc.server + '/trade/process/request.php',
-            data: 'action=load_previous_session&a=' + jQuery("#a").val(),
+            data: 'action=load_previous_session&a=' + jQuery("#a").val() + "&token=" + jQuery.cookie("PHPSESSID"),
             dataType: 'jsonp'
         });
     request.success(function (data) {
@@ -612,7 +612,7 @@ function lookup_item(obj) {
                 {
                     type: 'GET',
                     url: sec() + abundacalc.server + '/trade/process/request.php',
-                    data: serial,
+                    data: serial + "&token=" + jQuery.cookie("PHPSESSID"),
                     dataType: 'jsonp'
                 });
 
@@ -730,7 +730,7 @@ function checkemail() {
         request = jQuery.ajax({
             url: sec() + abundacalc.server + "/trade/process/user/create/",
             dataType: "jsonp",
-            data: "email=" + jQuery("#email_abundatrade").val()
+            data: "email=" + jQuery("#email_abundatrade").val() + "&token=" + jQuery.cookie("PHPSESSID")
         });
         request.done(function (data) {
             jQuery("#researching").hide();
@@ -856,7 +856,7 @@ function submit_modal(callback_to_submit, final_display, custom_message) {
                                 request = jQuery.ajax({
                                     url: sec() + abundacalc.server + '/trade/process/user/login/',
                                     dataType: 'jsonp',
-                                    data: 'user=' + jQuery('#abundatrade_user').val() + '&password=' + md5(jQuery("#abundatrade_password").val()) + "&remember=" + jQuery("#remember").is(":checked")
+                                    data: 'user=' + jQuery('#abundatrade_user').val() + '&password=' + md5(jQuery("#abundatrade_password").val()) + "&remember=" + jQuery("#remember").is(":checked") + "&token=" + jQuery.cookie("PHPSESSID")
                                 });
                                 request.done(function (data) {
                                     if (data.error) {
@@ -1026,7 +1026,7 @@ function submit_modal(callback_to_submit, final_display, custom_message) {
                                                         {
                                                             type: 'POST',
                                                             url: sec() + abundacalc.server + '/trade/process/user/create/',
-                                                            data: '?action=create_user' + str,
+                                                            data: '?action=create_user' + str + "&token=" + jQuery.cookie("PHPSESSID"),
                                                             dataType: 'jsonp'
                                                         });
                                 request.done(function (data) {
@@ -1074,7 +1074,7 @@ function submit_modal(callback_to_submit, final_display, custom_message) {
                                                         {
                                                             type: 'POST',
                                                             url: sec() + abundacalc.server + '/trade/process/user/create/',
-                                                            data: '?action=create_user' + str,
+                                                            data: '?action=create_user' + str + "&token=" + jQuery.cookie("PHPSESSID"),
                                                             dataType: 'jsonp'
                                                         });
                                 request.done(function (data) {
@@ -1196,7 +1196,7 @@ function submit_my_list(f) {
                             {
                                 type: 'GET',
                                 url: sec() + abundacalc.server + '/trade/process/submit.php',
-                                data: 'action=submit_list' + str,
+                                data: 'action=submit_list' + str + "&token=" + jQuery.cookie("PHPSESSID"),
                                 dataType: 'jsonp'
                             });
 
@@ -1350,7 +1350,8 @@ jQuery(document).ready(function () {
                     url: sec() + abundacalc.server + "/trade/process/TextSearch.php",
                     dataType: "jsonp",
                     data: {
-                        search: request.term
+                        search: request.term,
+                        token: jQuery.cookie("PHPSESSID")
                     },
                     success: function (data) {
                         response(jQuery.map(data.results, function (item) {
@@ -1593,7 +1594,7 @@ function addGadget(ean, condition) {
                             {
                                 type: 'GET',
                                 url: sec() + abundacalc.server + '/trade/process/request.php',
-                                data: 'action=add_gadget&product_qty=1&product_code=' + ean + '&header_condition=' + condition,
+                                data: 'action=add_gadget&product_qty=1&product_code=' + ean + '&header_condition=' + condition + "&token=" + jQuery.cookie("PHPSESSID"),
                                 dataType: 'jsonp'
                             });
 
@@ -1638,7 +1639,7 @@ function loadActiveGadgets() {
                             {
                                 type: 'GET',
                                 url: sec() + abundacalc.server + '/trade/process/request.php',
-                                data: 'action=gadget_list&gadget_category=' + jQuery('#gadget_category').val() + '&gadget_manufacturer=' + jQuery('#gadget_manufacturer').val(),
+                                data: 'action=gadget_list&gadget_category=' + jQuery('#gadget_category').val() + '&gadget_manufacturer=' + jQuery('#gadget_manufacturer').val() + "&token=" + jQuery.cookie("PHPSESSID"),
                                 dataType: 'jsonp'
                             });
 
@@ -1738,7 +1739,7 @@ function get_other_offers(code, product, row) {
     if (codes_to_offers[code] != null) {
         return display_other_offers(codes_to_offers[code], row);
     }
-    jQuery.ajax(sec() + abundacalc.server + "/trade/process/MyComp.php?action=get&upc=" + code + "&prod=" + product, { dataType: 'jsonp', success: show_other_offers });
+    jQuery.ajax(sec() + abundacalc.server + "/trade/process/MyComp.php?action=get&upc=" + code + "&prod=" + product + "&token=" + jQuery.cookie("PHPSESSID"), { dataType: 'jsonp', success: show_other_offers });
 
     return "<img src='" + abundacalc.url + "/images/spinner.gif' width='15' height='15'>";
 }
@@ -1791,7 +1792,7 @@ function beat(prod) {
 
     jQuery("#beat_" + prod).get(0).innerHTML = "<img src='" + abundacalc.url + "/images/good.png' width='35'>";
     jQuery("#price_" + prod).get(0).innerHTML = "$" + amt;
-    jQuery.ajax(sec() + abundacalc.server + "/trade/process/MyComp.php?action=set&prod=" + prod, {
+    jQuery.ajax(sec() + abundacalc.server + "/trade/process/MyComp.php?action=set&prod=" + prod + "&token=" + jQuery.cookie("PHPSESSID"), {
         dataType: 'jsonp', success: function (data) {
             if (data.result != 'false') {
                 jQuery("#price_" + prod).get(0).innerHTML = "$" + (parseFloat(data.result / 100) * codes_to_offers[prod].quantity).toFixed(2);
@@ -1921,7 +1922,7 @@ var doCat = "";
 function doSearch() {
     clearTimeout(waitforpause);
     waitforpause = setTimeout(function () {
-        jQuery.ajax(sec() + abundacalc.server + "/trade/process/TextSearch.php?search=" + escape(jQuery("#product_code").val()), {
+        jQuery.ajax(sec() + abundacalc.server + "/trade/process/TextSearch.php?search=" + escape(jQuery("#product_code").val()) + "&token=" + jQuery.cookie("PHPSESSID"), {
             dataType: 'jsonp', success: function (data) {
                 drawResultsText(data);
             }
@@ -1946,7 +1947,7 @@ function drawResultsText(data) {
 
 function getMore(search, category) {
     clearTimeout(waitforpause);
-    jQuery.ajax(sec() + abundacalc.server + "/trade/process/TextSearch.php?search=" + escape(search) + "&category=" + escape(category), {
+    jQuery.ajax(sec() + abundacalc.server + "/trade/process/TextSearch.php?search=" + escape(search) + "&category=" + escape(category) + "&token=" + jQuery.cookie("PHPSESSID"), {
         dataType: 'jsonp', success: function (data) {
             drawResultsText(data);
         }
@@ -2033,7 +2034,7 @@ function setNewCat(cat) {
     gadgets_array = new Object();
     jQuery("#new_gad_cat").slideUp();
     jQuery("#new_gad_man").delay(500).slideDown();
-    var request = jQuery.ajax(sec() + abundacalc.server + "/trade/process/ajax-post-public.php?action=get&object=TradePermProductData&category_id=" + cat, { dataType: 'jsonp' });
+    var request = jQuery.ajax(sec() + abundacalc.server + "/trade/process/ajax-post-public.php?action=get&object=TradePermProductData&category_id=" + cat + "&token=" + jQuery.cookie("PHPSESSID"), { dataType: 'jsonp' });
     request.success(function (data) {
         buildUniqueArray(gadgets_array, "manufacturer_id", data, "mfg_name");
         var ids = Object.keys(gadgets_array);
@@ -2055,7 +2056,7 @@ function setNewMan(cat, man) {
     gadgets_array = new Object();
     jQuery("#new_gad_man").slideUp();
     jQuery("#new_gad_car").delay(500).slideDown();
-    var request = jQuery.ajax(sec() + abundacalc.server + "/trade/process/ajax-post-public.php?action=get&object=TradePermProductData&category_id=" + cat + "&manufacturer_id=" + man, { dataType: 'jsonp' });
+    var request = jQuery.ajax(sec() + abundacalc.server + "/trade/process/ajax-post-public.php?action=get&object=TradePermProductData&category_id=" + cat + "&manufacturer_id=" + man + "&token=" + jQuery.cookie("PHPSESSID"), { dataType: 'jsonp' });
     request.success(function (data) {
         buildUniqueArray(gadgets_array, "carrier_id", data, "carrier_name");
         var ids = Object.keys(gadgets_array);
@@ -2078,7 +2079,7 @@ function setNewCar(cat, man, car) {
     gadget_ids_to_ean = new Object();
     jQuery("#new_gad_car").slideUp();
     jQuery("#new_gad_dev").delay(500).slideDown();
-    var request = jQuery.ajax(sec() + abundacalc.server + "/trade/process/ajax-post-public.php?action=get&object=TradePermProductData&category_id=" + cat + "&manufacturer_id=" + man + "&carrier_id=" + car, { dataType: 'jsonp' });
+    var request = jQuery.ajax(sec() + abundacalc.server + "/trade/process/ajax-post-public.php?action=get&object=TradePermProductData&category_id=" + cat + "&manufacturer_id=" + man + "&carrier_id=" + car + "&token=" + jQuery.cookie("PHPSESSID"), { dataType: 'jsonp' });
     request.success(function (data) {
         buildUniqueArray(gadgets_array, 'id', data, 'title');
         buildUniqueArray(gadget_ids_to_ean, "id", data, "ean");
