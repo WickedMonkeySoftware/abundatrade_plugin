bba6f84df9c4c0688033ae5f2b45a2f806d08ce9
Robert Landers <landers.robert@gmail.com>
2014-01-03 13:06:03 -0500

    Set distance correctly

--- js/remote.js
+++ js/remote.js
@@ -1586,7 +1586,7 @@ jQuery(document).ready(function () {
                     el.stop().animate({ 'top': 0 }, 200);
                     console.log("1");
                 }
-                else if (stopos - y < 50) {
+                else if (stopos - y < 65) {
                     el.stop().animate({ 'top': stopos - distance - elpos }, 200);
                     console.log("2");
                 }
