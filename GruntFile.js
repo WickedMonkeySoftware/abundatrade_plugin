fb7349b2fdbb89a9737721dafab1884251e1df87
Robert Landers <landers.robert@gmail.com>
2014-01-10 17:18:02 -0500

    Add grunt config (still doesn't work yet)

---
+++ GruntFile.js
@@ -0,0 +1,82 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'js/<%= pkg.jsFile %>.js',
+                dest: 'js/<%= pkg.jsFile %>.min.js'
+            }
+        },
+        qunit: {
+            all: ['tests/*.html']
+        },
+        watch: {
+            files: ['tests/*.js', 'tests/*.html', 'js/*.js'],
+            tasks: ['qunit']
+        },
+        php: {
+            dist: {
+                options: {
+                    port: 80,
+                    base: 'web',
+                    open: true,
+                    keepalive: true
+                }
+            }
+        },
+        phpcs: {
+            application: {
+                dir: 'src'
+            },
+            options: {
+                bin: 'phpcs',
+                standard: 'PSR-MOD'
+            }
+        },
+        phplint: {
+            options: {
+                swapPath: 'tmp'
+            },
+            all: ['*.php', 'skel/*.php', 'tabs/*.php']
+        },
+        phpunit: {
+            unit: {
+                dir: 'phptests/unit'
+            },
+            options: {
+                bin: 'phpunit',
+                bootstrap: 'phptests/Bootstrap.php',
+                colors: true,
+                testdox: true
+            }
+        },
+        php_analyzer: {
+            application: {
+                dir: '.'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //load php specific stuff
+    grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-php');
+    grunt.loadNpmTasks('grunt-phplint');
+    grunt.loadNpmTasks('grunt-phpunit');
+    grunt.loadNpmTasks('grunt-php-analyzer');
+
+    // Default task(s).
+    grunt.registerTask('default', ['qunit', 'uglify']);
+    grunt.registerTask('test', ['qunit']);
+    grunt.registerTask('precommit', ['phplint:all', 'phpunit:unit']);
+    grunt.registerTask('server', ['php']);
+};
